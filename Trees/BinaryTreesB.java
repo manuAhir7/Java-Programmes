48db391221f3d57268dffd18a6bc783e95970a3e
Manu Ahirwar <manuahirwar124@gmail.com>
2023-08-24 10:46:05 +0530

    Tree

--- Trees/BinaryTreesB.java
+++ Trees/BinaryTreesB.java
@@ -31,8 +31,7 @@ public class BinaryTreesB {
 
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(nodes);
+        Node root = BinaryTree.buildTree(nodes);
         System.out.println(root.data+" ");
     }
     
